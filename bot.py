4a1932bd6a3a07960eafe23ff221736d8e5495be
akbarzaaa358-ux <akbarzaaa358@gmail.com>
2026-05-26 20:11:03 +0700

    Fix indentation for time retrieval in bot.py

--- bot.py
+++ bot.py
@@ -680,7 +680,7 @@ async def tambahmasaaktif(update, context):
 
     add_days = int(context.args[0])
     name = " ".join(context.args[1:]).lower()
-        now = time.time()
+    now = time.time()
 
     for g in groups_col.find():
         for uid, data in g.get("premium_users", {}).items():
